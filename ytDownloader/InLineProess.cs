a390ae08ebfa4fd87961a0b727ea62a282ccfa62
LukeTBurton <72513543+LukeTBurton@users.noreply.github.com>
2020-10-07 17:20:34 +0100

    Add files via upload

---
+++ ytDownloader/InLineProess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ytDownloader
+{
+    internal class InlineProgress : IProgress<double>, IDisposable
+    {
+        private readonly int _posX;
+        private readonly int _posY;
+
+        public InlineProgress()
+        {
+            _posX = Console.CursorLeft;
+            _posY = Console.CursorTop;
+        }
+
+        public void Report(double progress)
+        {
+            Console.SetCursorPosition(_posX, _posY);
+            Console.WriteLine($"{progress:P1}");
+        }
+
+        public void Dispose()
+        {
+            Console.SetCursorPosition(_posX, _posY);
+            Console.WriteLine("Completed ✓");
+        }
+    }
+}
